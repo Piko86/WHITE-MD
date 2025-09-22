b4e59e033ad45b06c7c720d850faa1235c546e58
Piko86 <manmithabackup@gmail.com>
2025-09-22 22:24:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nc9VVaxJ#yWXW1-YB_LSj7Xgrx6bZJCkEhErob5qGWZ0o0XhGGxQ",
+SESSION_ID: process.env.SESSION_ID || "SMshnZSI#ZJuFUoEMt6RdlPc51D3YE3AXXMwCgJeFKzMWaNbRq_A",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM HASHAN MD CREATED BY HASHAN <NOW ALIVE> ",
 };
